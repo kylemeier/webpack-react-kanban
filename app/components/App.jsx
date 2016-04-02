5114f54071b9364f9d2984f4435ade8b6dede089
Kyle Meier <kyledmeier@gmail.com>
2016-04-02 16:54:31 -0700

    added alt and now using flux

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,30 +1,28 @@
-import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
+
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
 
 	constructor(props){
 		super(props);
 
-		this.state = {
-			notes: [
-				{
-					id: uuid.v4(),
-					task: 'Learn Webpack'
-				},
-				{
-					id: uuid.v4(),
-					task: 'Learn React'
-				},
-				{
-					id: uuid.v4(),
-					task: 'Do laundry'
-				}
-			]
-		};
-
+		this.state = NoteStore.getState();
 	}
+
+	componentDidMount(){
+		NoteStore.listen(this.storeChanged);
+	}
+
+	componenetWillUnmount(){
+		NoteStore.unlisten(this.storeChanged);
+	}
+
+	storeChanged = (state) => {
+		this.setState(state);
+	};
 
 	render() {
 		const notes = this.state.notes;
@@ -39,32 +37,19 @@ export default class App extends React.Component {
 		);
 	}
 
-	addNote = () => {
-		this.setState({
-			notes: this.state.notes.concat([{
-				id: uuid.v4(),
-				task: 'New task'
-			}])
-		});
+	addNote(){
+		NoteActions.create({task: 'New task'});
 	}
 
-	editNote = (id, task) => {
-		if(!task.trim()) return;
-		const editedNoteIndex = this.state.notes.findIndex( note => note.id === id );
-		const noteToEdit = this.state.notes.find( note => note.id === id );
-		const editedNote = Object.assign({}, noteToEdit, {task: task});
-		const notes = this.state.notes
-										.slice(0,editedNoteIndex)
-										.concat([editedNote])
-										.concat( this.state.notes.slice(editedNoteIndex) );
+	editNote(id, task){
+		if( !task.trim() ) return;
 
-		this.setState({notes});
+		NoteActions.update({id, task});
 	};
 
 	deleteNote = (id, e) => {
 		e.stopPropagation();
-		this.setState({
-			notes: this.state.notes.filter(note => note.id !== id)
-		});
+		
+		NoteActions.delete(id);
 	};
 }
