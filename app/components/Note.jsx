29f54d28bcd299d363afccd0173ec3b5b6160954
Kyle Meier <kyledmeier@gmail.com>
2016-04-12 00:19:06 -0700

    added note drag functionality

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -2,69 +2,26 @@ import React from 'react';
 
 import style from '../Note.css';
 
+import {DragSource} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
+
+const noteSource = {
+	beginDrag(props){
+		console.log('begin dragging note', props);
+
+		return {};
+	}
+};
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+	connectDragSource: connect.dragSource()
+}))
 export default class Note extends React.Component {
-	constructor(props){
-			super(props);
-
-			this.state = {
-				editing: false
-			};
-	}
-
 	render(){
+		const {connectDragSource, id, onMove, ...props} = this.props;
 
-		if(this.state.editing){
-			return this.renderEdit();
-		}
-		else{
-			return this.renderNote();
-		}
-	}		
-
-	renderEdit = () => {
-
-		return <input type="text"
-			ref={
-				(e) => e ? e.selectionStart = this.props.task.length : null
-			}
-			autoFocus={true}
-			defaultValue={this.props.task}
-			onBlur={this.finishEdit}
-			onKeyPress={this.checkEnter} />;
-	};
-
-	renderNote = () => {
-
-		const onDelete = this.props.onDelete;
-
-		return (
-			<div onClick={this.edit}>
-				<span className={styles.task}>{this.props.task}</span>
-				{onDelete ? this.renderDelete() : null }
-			</div>
+		return connectDragSource(
+			<li {...this.props}>{this.props.children}</li>
 		);
-	};
-
-	renderDelete = () => {
-		return <button className={style.btn} onClick={this.props.onDelete}>x</button>;
-	};
-
-	edit = () => {
-		this.setState({editing:true});
-	};
-
-	checkEnter = (e) => {
-		if(e.key === 'Enter') {
-			this.finishEdit(e);
-		}
-	}
-
-	finishEdit = (e) => {
-		const value = e.target.value;
-
-		if(this.props.onEdit){
-			this.props.onEdit(value);
-			this.setState({editing: false});
-		}
 	}
 }
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Editable from './Editable.jsx';
+import Note from './Note.jsx';
 
 import notesStyles from '../Notes.css';
 import noteStyles from '../Note.css';
@@ -7,14 +8,14 @@ import noteStyles from '../Note.css';
 export default ({notes, onValueClick, onEdit, onDelete}) => {
 	return (
 		<ul className={notesStyles.root}>{notes.map(note => 
-			<li className={noteStyles.root} key={note.id}>
-				<Editable
-					editing={note.editing}
-					value={note.task}
-					onValueClick={onValueClick.bind(null, note.id)}
-					onEdit={onEdit.bind(null, note.id)}
-					onDelete={onDelete.bind(null, note.id)} />
-			</li>
-			)}</ul>
+			<Note className={noteStyles.root} id={note.id} key={note.id}>
+					<Editable
+						editing={note.editing}
+						value={note.task}
+						onValueClick={onValueClick.bind(null, note.id)}
+						onEdit={onEdit.bind(null, note.id)}
+						onDelete={onDelete.bind(null, note.id)} />
+			</Note>
+		)}</ul>
 		);
 }
