d3bd63d6e19a205244dd33014a77df55d018d0f8
Kyle Meier <kyledmeier@gmail.com>
2016-04-14 23:59:29 -0700

    make notes actually move

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -5,14 +5,14 @@ import Note from './Note.jsx';
 import notesStyles from '../Notes.css';
 import noteStyles from '../Note.css';
 
+import LaneActions from '../actions/LaneActions';
+
 export default ({notes, onValueClick, onEdit, onDelete}) => {
 	return (
 		<ul className={notesStyles.root}>{notes.map(note =>  {
 			return(
 				<Note className={noteStyles.root} id={note.id} key={note.id}
-					onMove={({sourceId, targetId}) =>
-						console.log('source: ${sourceId}, target: ${targetId}')
-					}>
+					onMove={LaneActions.move}>
 						<Editable
 							editing={note.editing}
 							value={note.task}
